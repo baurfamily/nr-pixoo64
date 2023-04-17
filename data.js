c61fd499eef359289ed4b3c1c38a6485db85862c
Eric Baur <baurfamily@gmail.com>
2023-04-17 16:42:12 -0700

    more stuff

--- data.js
+++ data.js
@@ -1,24 +1,44 @@
 import fetch from 'cross-fetch'
-import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client/core/core.cjs"
 
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: new HttpLink({ uri: 'https://flyby-router-demo.herokuapp.com/', fetch })
-})
-
-// const client = ...
-
-client
-  .query({
-    query: gql`
-      query GetLocations {
-        locations {
-          id
-          name
-          description
-          photo
+const entities = async () => {
+  const body = JSON.stringify({
+    query: `
+      query {
+        actor {
+          entitySearch(query:"type IN ( 'APPLICATION', 'MONITOR', 'KEY_TRANSACTION')") {
+            results {
+              entities {
+                name
+                guid
+                type
+                domain
+              }
+            }
+          }
         }
       }
-    `,
+    `
   })
-  .then((result) => console.log(result));
+
+  const options = {
+    body,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Api-Key': process.env.API_KEY
+    }
+  }
+
+  const resp = await fetch(
+    'https://api.newrelic.com/graphql',
+    options
+  ).catch(err => {
+    console.error(err);
+  });
+
+  const result = await resp.json()
+
+  return result.data.actor.entitySearch.results.entities
+}
+
+console.log(await entities())
--- index.js
+++ index.js
@@ -8,25 +8,10 @@ await pixoo.initialize()
 pixoo.clear()
 pixoo.fill(colors.black)
 
-
-// HORIZONTAL LAYOUT
-// const titlePadding = 0
-
-// pixoo.drawTextLeft("APM", 0, colors.white, titlePadding )
-// pixoo.drawRect( [ 0, 6 ], [ 63, 21 ], colors.white, false )
-
-// pixoo.drawTextLeft("Synthetics", 22, colors.white, titlePadding )
-// pixoo.drawRect( [ 0, 28 ], [ 63, 42 ], colors.white, false )
-
-// pixoo.drawTextLeft("SLOs", 43, colors.white, titlePadding )
-// pixoo.drawRect( [ 0, 49 ], [ 63, 63 ], colors.white, false )
-
-// pixoo.push()
-
-const fillFrom = (start) => {
-  for (var i=0; i<6; i++) {
+const fillFrom = (start, cols, rows) => {
+  for (var i=0; i<cols; i++) {
     const col = start[0] + i*3
-    for (var j=0; j<18; j++) {
+    for (var j=0; j<rows; j++) {
       const row = start[1] + j*3
       pixoo.drawRect( [ col, row ], [ col+2, row+2 ], colors.green, true )
     }
@@ -34,47 +19,22 @@ const fillFrom = (start) => {
   pixoo.push()
 }
 
-fillFrom([ 2, 8 ])
-fillFrom([ 23, 8 ])
-fillFrom([ 44, 8 ])
+fillFrom([ 2, 8 ], 6, 18)
+fillFrom([ 23, 8 ], 6, 8)
+fillFrom([ 23, 41 ], 6, 7)
+fillFrom([ 44, 8 ], 6, 18 )
 
 
-pixoo.drawTextLeft("APM", 0, colors.white, 0 )
-pixoo.drawRect( [ 0, 6 ], [ 21, 63 ], colors.white, false )
+pixoo.drawTextLeft("APM", 1, colors.white, 1 )
+pixoo.drawRect( [ 0, 6 ], [ 21, 63 ], colors.grey, false )
 
-pixoo.drawTextLeft("Syn", 0, colors.white, 21 )
-pixoo.drawRect( [ 21, 6 ], [ 42, 63 ], colors.white, false )
+pixoo.drawTextLeft("SYN", 1, colors.white, 22 )
+pixoo.drawRect( [ 21, 6 ], [ 42, 33 ], colors.grey, false )
 
-pixoo.drawTextLeft("SLOs", 0, colors.white, 42 )
-pixoo.drawRect( [ 42, 6 ], [ 63, 63 ], colors.white, false )
+pixoo.drawTextLeft("KTs", 34, colors.white, 22 )
+pixoo.drawRect( [ 21, 39 ], [ 42, 63 ], colors.grey, false )
+
+pixoo.drawTextLeft("SLOs", 1, colors.white, 43 )
+pixoo.drawRect( [ 42, 6 ], [ 63, 63 ], colors.grey, false )
 
 pixoo.push()
-
-// for( var i=0; i <= 1000; i++ ) {
-//  pixoo.drawPixel(
-//    Math.floor(i / 64),
-//    i % 64,
-//    [0, 255, 0]
-//  )
-//   pixoo.push()
-//   await sleep(1000)
-// }
-
-
-// while (true) {
-//   const start = [ randomInt(64), randomInt(64) ]
-//   const end = [ randomInt(64), randomInt(64) ]
-
-//   const white = [ 255, 255, 255 ]
-//   const color = [ randomInt(255), randomInt(255), randomInt(255) ]
-
-//   pixoo.drawPixel( ...start, white )
-//   pixoo.drawPixel( ...end, white )
-
-//   // pixoo.drawLine( start, end, color )
-//   pixoo.drawTextLeft("Foo", start[0], color, start[1] )
-//   pixoo.drawTextRight("Bar", end[0], color, end[1] )
-//   pixoo.push()
-
-//   await sleep(1000)
-// }
